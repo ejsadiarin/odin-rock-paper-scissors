d40304cee835a25e20350bc2b5e18a02ed523891
ejsadiarin <ejsadiarin@gmail.com>
2023-02-01 18:57:20 +0800

    finished console app RPS, but no custom input

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const choices = ["rock", "paper", "scissors"];
 
 // Human input
 function getHumanInput(){
-    const input = prompt("Choose your fate: Rock, Paper, or Scissors?").trim().toLowerCase(); // need input
+    // const input = prompt("Choose your fate: Rock, Paper, or Scissors?").trim().toLowerCase(); // need input
+    const input = "Rock".toLowerCase().trim();
 
     if (choices.includes(input)) {
         return input;
     } else {
-        alert("There's no such choice. Try again.");
+        // alert("There's no such choice. Try again.");
+        console.log("There's no such choice. Try again.")
         getHumanInput();
     }
 } 
@@ -35,24 +37,26 @@ function evaluate() {
     const human = getHumanInput();
     const computer = getComputerChoice(); 
     if (human === computer) {
-        console.log("Tie.")
+        return console.log("Tie.")
     }
     if (human === "rock" && computer === "scissors") {
-        console.log("You win! Rock beats scissors.");
+        return console.log("You win! Rock beats scissors.");
     }
     if (human === "rock" && computer === "paper") {
-        console.log("You lose! Paper beats rock.");
+        return console.log("You lose! Paper beats rock.");
     }
     if (human === "paper" && computer === "scissors") {
-        console.log("You lose! Paper beats scissors.");
+        return console.log("You lose! Paper beats scissors.");
     }
     if (human === "paper" && computer === "rock") {
-        console.log("You win! Paper beats rock.");
+        return console.log("You win! Paper beats rock.");
     }
     if (human === "scissors" && computer === "rock") {
-        console.log("You lose! Rock beats scissors.");
+        return console.log("You lose! Rock beats scissors.");
     }
     if (human === "scissors" && computer === "paper") {
-        console.log("You win! Scissors beats paper.");
+        return console.log("You win! Scissors beats paper.");
     }
 }
+
+evaluate();
