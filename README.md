2d5554383ddb7a7d0934ae6c3d7f7d9aaf047cf0
ejsadiarin <ejsadiarin@gmail.com>
2023-02-01 16:24:34 +0800

    added files, game logic in js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-rock-paper-scissors
+Started February 1, 2023 3:43 PM
+
+## 
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// 
+
+
+// Human input
+const humanInput = prompt("Choose your fate: Rock, Paper, or Scissors?"); // need input
+
+if (humanInput.trim().toLowerCase())
+
+// Computer choice
+function getRandomNumber() {
+    const random = Math.floor(Math.random() * 3 + 1);
+
+    return random;
+}
+
+function getComputerChoice {
+    const choices = ["Rock", "Paper", "Scissors"];
+    let randomNumber = getRandomNumber();
+    let computerChoice = choices[randomNumber];
+
+    if (computerChoice === choices[0]) {
+        
+    }
+}
+
+// compare choice and who wins
+function evaluate() {
+    const human = humanInput.trim().toLowerCase();
+    const computer = getComputerChoice(); 
+    if (humanInput === ) {
+        
+    }
+}
